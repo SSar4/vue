3ba473ae8615bf7becf50faca5a452c4ff40f92c
Fulano de Tal <aras.strong8@gmail.com>
2021-09-04 21:32:10 -0300

    s2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var path = require('path')
-var serverStatic = require('serve-static')
+var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serverStatic(path.join(__dirname,'dist')))
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = 5000
-
-app.listen(process.env.PORT || port)
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
